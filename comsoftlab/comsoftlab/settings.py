599f770b65e8d0b1e93d22e332eec33c0045e13c
nk-knvlv <nk_knvlv@mail.ru>
2024-09-08 20:57:06 +0300

    update; settings.py, static/, templates, style.css; стили статика, отдельный модуль для мыла, шаблоны и новые настройки

--- comsoftlab/comsoftlab/settings.py
+++ comsoftlab/comsoftlab/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Если у вас есть общая папка для статиков
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
